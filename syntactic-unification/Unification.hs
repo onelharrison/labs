0f90f37d57ed8a48311a2388b594484c48ef748c
Onel Harrison <onelharrison@users.noreply.github.com>
2020-11-28 03:05:54 -0500

    Add unification support for composite terms

--- syntactic-unification/Unification.hs
+++ syntactic-unification/Unification.hs
@@ -4,14 +4,12 @@ import qualified Data.Map as Map
 
 data UTerm = UNum Int
     | UVar String
+    | UList [UTerm]
     deriving (Show, Eq, Ord)
 
 type Substitution = Map.Map UTerm UTerm
 
 unify :: UTerm -> UTerm -> Substitution -> Maybe Substitution
-unify (UNum a) (UNum b) s
-  | a == b = Just s
-  | otherwise = Nothing
 unify a (UVar b) s
   | a' == b' = Just s
   | occurs a' b' s = Nothing
@@ -26,13 +24,23 @@ unify (UVar a) b s
   where
     a' = walk (UVar a) s
     b' = walk b s
+unify (UList []) (UList []) s = Just s
+unify (UList (x:xs)) (UList (y:ys)) s =
+  case unify x y s of
+    Just s' -> unify (UList xs) (UList ys) s'
+    Nothing -> Nothing
+unify (UList _) (UList _) s = Nothing
+unify a b s
+  | a == b = Just s
+  | otherwise = Nothing
 
 walk :: UTerm -> Substitution -> UTerm
-walk (UNum a) s = UNum a
 walk (UVar x) s =
   case Map.lookup (UVar x) s of
-    Just xv -> walk xv s
+    Just v  -> walk v s
     Nothing -> UVar x
+walk a _ = a
 
 occurs :: UTerm -> UTerm -> Substitution -> Bool
-occurs a b s = a == walk b s
+occurs a (UList bs) s = any (\b -> occurs a b s) bs
+occurs a b s          = a == walk b s
