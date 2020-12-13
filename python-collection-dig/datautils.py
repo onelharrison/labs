357a7783bd8cf13456f552f50d39ab64ecbb3bf9
Onel Harrison <onelharrison@users.noreply.github.com>
2020-12-13 12:35:27 -0500

    Add types and docstrings to datautils.py functions

--- python-collection-dig/datautils.py
+++ python-collection-dig/datautils.py
@@ -1,11 +1,21 @@
+"""A collection of functions for safely working with dirty data"""
+
 from functools import reduce
+from typing import (Any, Callable, Collection, Hashable, Iterable, List,
+                    Mapping, Optional, Sequence, Union, overload)
 
 
-def coalesce(*values, default=None):
+def coalesce(
+    *values: Iterable[Optional[Any]], default: Optional[Any] = None
+) -> Optional[Any]:
+    """Return the first non-None value"""
     return next((v for v in values if v is not None), default)
 
 
-def safe_cast(value, astype, default=None):
+def safe_cast(
+    value: Any, astype: Callable, default: Optional[Any] = None
+) -> Optional[Any]:
+    """Convert one type to another without raising errors"""
     try:
         return astype(value)
     except (TypeError, ValueError):
@@ -14,10 +24,24 @@ def safe_cast(value, astype, default=None):
     return default
 
 
-def safe_get(collection, key, default=None):
-    if isinstance(collection, dict):
+@overload
+def safe_get(collection: Mapping, key: Hashable, default: Optional[Any] = None):
+    pass
+
+
+@overload
+def safe_get(collection: Sequence, key: int, default: Optional[Any] = None):
+    pass
+
+
+def safe_get(collection, key, default = None):
+    """Get values from a collection without raising errors"""
+    # pylint:disable=isinstance-second-argument-not-valid-type
+
+    if isinstance(collection, Mapping):
         return collection.get(key, default)
-    elif isinstance(collection, list):
+
+    if isinstance(collection, Sequence):
         try:
             return collection[key]
         except (IndexError, TypeError):
@@ -26,5 +50,10 @@ def safe_get(collection, key, default=None):
     return default
 
 
-def dig(collection, *keys, default=None):
-    return reduce(lambda x, y: safe_get(x, y, default), keys, collection)
+def dig(
+    collection: Collection,
+    *keys: List[Union[int, Hashable]],
+    default: Optional[Any] = None
+) -> Optional[Any]:
+    """Get values from a potentially nested collection without raising errors"""
+    return reduce(lambda x, y: safe_get(x, y, default), keys, collection)  # type: ignore
