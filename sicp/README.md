f8d4dc7eddc8128439d5d183cce2d5066fab3e79
Onel Harrison <onelharrison@users.noreply.github.com>
2021-07-01 09:15:58 -0400

    Add solution for sicp ex1-1

--- sicp/README.md
+++ sicp/README.md
@@ -1,3 +1,12 @@
 # Structure and Interpretation of Computer Programs
 
 Exercise solutions to problems presented in Structure and Interpretation of Computer Programs (2nd ed.)
+
+## Getting started
+
+1. Install Racket
+
+2. Instlal the sicp Racket package
+    ```
+    raco pkg install sicp
+    ```
--- sicp/chpt1/exercises/ex1-1.rkt
+++ sicp/chpt1/exercises/ex1-1.rkt
@@ -1,1 +1,54 @@
 #lang sicp
+
+; Exercise 1.1
+;
+; Below is a sequence of expressions. What is the result printed
+; by the interpreter in response to each expression? Assume that
+; the sequence is to be evaluated in the order in which it is
+; presented.
+
+; Ans: 10
+10
+
+; Ans: 12
+(+ 5 3 4)
+
+; Ans: 8
+(- 9 1)
+
+; Ans: 3
+(/ 6 2)
+
+; Ans: 6
+(+ (* 2 4) (- 4 6))
+
+; Ans: no output
+(define a 3)
+
+; Ans: no output
+(define b (+ a 1))
+
+; Ans: 19
+(+ a b (* a b))
+
+; Ans: #f
+(= a b)
+
+; Ans: 4
+(if (and (> b a) (< b (* a b)))
+  b
+  a)
+
+; Ans: 16
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+
+; Ans: 6
+(+ 2 (if (> b a) b a))
+
+; Ans: 16
+(* (cond ((> a b) a)
+	 ((< a b) b)
+	 (else -1))
+   (+ a 1))
